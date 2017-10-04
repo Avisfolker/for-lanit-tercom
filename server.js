64d1b9cf6c84f2f624cb616bddd0aa6fa0570951
andreynorth86 <andreynorth86@gmail.com>
2017-10-04 14:17:54 +0300

    'startProject'

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const request = require('request');
+const app = express();
+let apiRoutes = express.Router();
+
+apiRoutes.use((req, res, next) => { //allow cross origin requests
+  res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+res.header("Access-Control-Allow-Origin", "http://localhost");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
+
+apiRoutes.use(bodyParser.json());
+
+app.use('/api', apiRoutes);
+// API file for interacting with MongoDB
+// const api = require('./server/routes/api');
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// API location
+// app.use('/api', api);
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+
+apiRoutes.get('/getSearch', (req, res) => { // получаем запрос
+   // 6230014f6230014f62158a6674626aaaf4662306230014f3ae3c1d8026399365abcbffb
+  console.log(req.query);
+  console.log(
+    'https://api.vk.com/method/newsfeed.search?' +
+    'q=%23' + req.query.searchWord + '&' +
+    'access_token=4033e9204033e920406e50337340612122440334033e920189808ef9aa4f4e57a7694da&' +
+    'count=10&' +
+    'v=5.68'
+
+  );
+  request.get('https://api.vk.com/method/newsfeed.search?' +
+    'q=%23' + req.query.searchWord + '&' +
+    'access_token=4033e9204033e920406e50337340612122440334033e920189808ef9aa4f4e57a7694da&' +
+    'count=10&' +
+    'v=5.68'
+    ,(error,response,body) => {
+      if (!error && response.statusCode === 200) {
+        res.send(body);
+      }
+      else {
+        res.send({success: false, msg: error});
+      }
+
+
+  });
+});
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {isUndefined} from "util";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private http: HttpClient) {}
+  searchWord: string;
+  searchData: any = [];
+  searchErr: any = false;
+
+
+  setValue() {
+    this.searchErr = false;
+      this.http.get('/api/getSearch?searchWord=' + this.searchWord).subscribe((data: any) => {
+        console.log(data);
+        console.log(data.response);
+        if (data.success === false) {
+          this.searchData = [];
+          this.searchErr = 'Ошибка, попробуйте другое слово.';
+        } else {
+          if (data.response.count === 0) {
+            this.searchData = [];
+            this.searchErr = 'По вашему запросу ничего не найдено.';
+          } else {
+            this.searchData = data.response.items;
+          }
+        }
+
+    });
+
+
+
+  }
+  delValue(id){
+    for(let i = 0;i < this.searchData.length; i++ ) {
+      if(this.searchData[i].id === id) {
+        this.searchData.splice(i,1);
+      }
+    }
+
+
+  }
+
+
+  title = 'приложение';
+}
